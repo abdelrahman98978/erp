22003fce27c377e60bf20f67d933fd3f570572bd
abdelrahman98978 <abdelrahman98978@users.noreply.github.com>
2026-08-01 16:51:19 +0300

    feat: initialize enterprise resource planning application with core modules, i18n support, and automated login utility

---
+++ dist/assets/Badge-DyX45TGr.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-Dh-paq3M.js";const i=({text:e,type:r="info",icon:s})=>a.jsxs("span",{className:`badge-pill ${r}`,children:[s&&a.jsx("i",{className:s}),e]});export{i as B};
